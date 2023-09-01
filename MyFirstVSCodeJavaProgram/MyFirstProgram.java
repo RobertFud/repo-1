dac095ca5ed0a4d3d5244335fc7847800a9b7a00
Robert <robr100604@gmail.com>
2023-09-01 10:47:21 -0500

    Added a folder for class

---
+++ MyFirstVSCodeJavaProgram/MyFirstProgram.java
@@ -0,0 +1,9 @@
+package MyFirstVSCodeJavaProgram;
+
+public class MyFirstProgram
+{
+    public static void main(String[] args)
+    {
+        System.out.println("ITS FRIDAYYYYYY!!!!!!");
+    }
+}
